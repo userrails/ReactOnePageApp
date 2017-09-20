d7a83099bb2cb7aef3f4b059dd3cb8c2031975f1
Shiv Raj Badu <shivrajbadu@gmail.com>
2017-09-20 11:41:45 +0545

    Routes.js code is set

---
+++ src/pages/About.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+class About extends React.Component {
+  render() {
+    return <div>This is about page</div>;
+  }
+}
---
+++ src/pages/Contact.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+class Contact extends React.Component {
+  render() {
+    return <div>This is contact page</div>;
+  }
+}
