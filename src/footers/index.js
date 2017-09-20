eafab1b9fcb2fff83ccc1f42fc6c992ed21fbd75
Shiv Raj Badu <shivrajbadu@gmail.com>
2017-09-20 15:45:23 +0545

    Worked on footer section

---
+++ src/footers/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+import './index.css'
+
+export default class Footers extends React.Component {
+  render() {
+    return (
+      <div>
+        <div className="footer-section">
+          <div className="container">
+            <div className="row">
+              <div className="col-sm-12">
+                <h2>hello is footer section</h2>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
--- src/index.js
+++ src/index.js
@@ -3,9 +3,11 @@ import ReactDOM from 'react-dom';
 
 import './index.css';
 import Headers from './Headers';
+import Footers from './footers';
 
 ReactDOM.render(
 	<div>
     <Headers/>
+    <Footers/>
   </div>, document.getElementById('root')
 );
