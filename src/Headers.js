c03f941d574000c682f1ad15ba9d4850d5c6627a
Shiv Raj Badu <shivrajbadu@gmail.com>
2017-09-20 14:30:52 +0545

    adding bootstrap navigation

--- src/Headers.js
+++ src/Headers.js
@@ -9,13 +9,19 @@ export default () => {
   return(
     <BrowserRouter>
       <div>
-        <div>
-          <p><Link to="/">Home</Link></p>
-          <p><Link to="/contact">Contact</Link></p>
-          <p><Link to="/about">About</Link></p>
+        <div className="navbar navbar-default">
+          <div className="container">
+            <div className="navbar-header pull-left">
+              <ul className="nav navbar-nav">
+                <li className="active"><Link to="/">Home<span className="sr-only">(current)</span></Link></li>
+                <li><Link to="/contact">Contact</Link></li>
+                <li><Link to="/about">About</Link></li>
+              </ul>
+            </div>
+          </div>
         </div>
+      
         <Switch>
-          
           <Route exact path="/" component={Home} />
           <Route exact path="/contact" component={Contact} />
           <Route exact path="/about" component={About} />
